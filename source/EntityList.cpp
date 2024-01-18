220e2d5f3dd17530848e6400a5824221f039aee6
Vlad <alpatof74@gmail.com>
2024-01-18 18:51:43 +0300

    added sigscanner

--- source/EntityList.cpp
+++ source/EntityList.cpp
@@ -4,20 +4,35 @@
 
 #include "titanfall2_sdk/EntityList.h"
 #include <Windows.h>
+#include <sigscanner/ModuleScanner.h>
+#include <stdexcept>
+#include <format>
 
+
+static std::string clientEntityListSignature = "48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8D 05 ? ? ? ? 48 8D 0D ? ? ? ? 0F 57 "
+                                               "C0 48 89 05 ? ? ? ? 48 8D 05 ? ? ? ? 45 33 C0";
 
 namespace titanfall2_sdk
 {
 
     const EntityList& EntityList::Get()
     {
-        static const auto& entList = *reinterpret_cast<const EntityList*>((uintptr_t)GetModuleHandleA("client.dll") + 0xDC9E28);
+        static const auto& scanResult =
+                signature_scanner::ModuleScanner("client.dll").FindPattern(clientEntityListSignature);
 
-        return entList;
+        if (!scanResult.has_value())
+            throw std::runtime_error(std::format("Failed to find entity list! Pattern: '{}'",
+                                                 clientEntityListSignature));
+
+        const auto offsetToList = *(uint32_t*)(scanResult.value()+3);
+
+        return *reinterpret_cast<EntityList*>(scanResult.value()+offsetToList+7);
     }
 
     BaseEntity *EntityList::GetEntityById(size_t id)
     {
-        return GetByOffset<BaseEntity*>(static_cast<std::ptrdiff_t>(id) * 0x20);
+        constexpr auto vtableSize = sizeof(uintptr_t);
+
+        return GetByOffset<BaseEntity*>(static_cast<std::ptrdiff_t>(id) * 0x20 + vtableSize);
     }
 } // titanfall2_sdk
